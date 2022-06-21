b9ab2f2a04bed275a5bf8e1ae028edced6a2c955
jkkim74 <jack68@naver.com>
2022-06-21 16:49:41 +0900

    new vue-todo2

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodoItem">
+        <button v-on:click="addTodo">add</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            newTodoItem: ""
+        }
+    },
+    methods:{
+        addTodo: function(){
+            console.log(this.newTodoItem);
+            //저장로직수행
+            localStorage.setItem(this.newTodoItem,this.newTodoItem);
+            this.newTodoItem = "";
+        }
+    }
+}
+</script>
+
+<style>
+</style>
