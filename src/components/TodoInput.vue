89e05da42f8d36508cbca2247ed1873539c5c837
jack <jack68@naver.com>
2022-06-29 15:16:56 +0900

    component implentation

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,8 +1,10 @@
 <template>
-    <div>
-        <input type="text" v-model="newTodoItem">
-        <button v-on:click="addTodo">add</button>
-    </div>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
 </template>
 
 <script>
@@ -14,14 +16,44 @@ export default {
     },
     methods:{
         addTodo: function(){
-            console.log(this.newTodoItem);
-            //저장로직수행
-            localStorage.setItem(this.newTodoItem,this.newTodoItem);
+          if(this.newTodoItem !== ''){
+            var obj = {completed: false, item: this.newTodoItem}
+              console.log(this.newTodoItem);
+              //저장로직수행
+              localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+              this.clearInput();
+          }
+        },
+        clearInput: function(){
             this.newTodoItem = "";
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,84 @@
 <template>
   <div>
-    List
+   <ul>
+    <li v-for="(todoItem, index) in todoItems " v-bind:key="todoItem" class="shadow">
+    <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem,index)"></i>
+    <span v-bind:class="{textCompleted: todoItem.completed}">{{todoItem.item}}</span>
+    <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+      <i class="removeBtn fas fa-trash-alt"></i>
+    </span>
+    </li>
+   </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: function(){
+    return {
+      todoItems: []
+    }
+  },
+  methods:{ 
+    removeTodo: function(todoItem, index){
+      console.log(todoItem, index);
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index,1);
+    },
+    toggleComplete: function(todoItem,index){
+      console.log(todoItem,index);
+      todoItem.completed = !todoItem.completed;
+      // localStorage는 업데이트가 없으므로 해당 ITEM을 삭제 후, 재정의하여 업데이트 처리를 한다..
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item,JSON.stringify(todoItem));
+    }
+  },
+  created:function(){
+      if(localStorage.length >= 0){
+        for(var i = 0 ; i < localStorage.length ; i ++){
+          if(localStorage.key(i) !== "loglevel:webpack-dev-server"){
+            //this.todoItems.push(localStorage.key(i));
+            // console.log(localStorage.key(i));
+            this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+          }
+        }
+      }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  /* color: black; */
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  /* color: #62acde; */
+  color: black;
+}
+.textCompleted {
+  text-decoration: line-through;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
 </style>
